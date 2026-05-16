bd96001da2c334d48ce482cefea2ea51b9e9d172
Lailo <748525+lailo@users.noreply.github.com>
2026-05-16 20:33:31 +0200

    lly v0.1.0

--- Formula/lly.rb
+++ Formula/lly.rb
@@ -1,9 +1,9 @@
 class Lly < Formula
   desc "TUI for Shelly Gen2/Gen3 devices to discover, manage, and interact with"
   homepage "https://github.com/lailo/lly"
-  url "https://github.com/lailo/lly/releases/download/v0.0.0/lly-darwin-arm64.tar.gz"
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  url "https://github.com/lailo/lly/releases/download/v0.1.0/lly-darwin-arm64.tar.gz"
+  version "0.1.0"
+  sha256 "a24f0181fc554264b4b7e5a0e12d66e919a161b635c801685f4355a259d52910"
   license "MIT"
 
   depends_on arch: :arm64
